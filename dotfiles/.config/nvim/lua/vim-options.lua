42f4a33888ec11d6749bbc9642d6363c64635297
madfury@arch <kelvinkarani11934@gmail.com>
2025-08-03 01:58:22 +0300

    <Feature> Neovim configs
    The commit adds lazy.nvim as the packet manager
    Also adds catppuccin as the colour scheme
    Also adds lualine, neotree, telescope and treesitter plugins
    
    Vim editor settings are also added and mapleader set to " "

---
+++ dotfiles/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.cmd( "set number" )
+vim.cmd( "set relativenumber" )
+vim.cmd( "set wrap!" )
+vim.cmd( "set expandtab" )
+vim.cmd( "set shiftwidth=4" )
+vim.cmd( "set softtabstop=4" )
