2e7229c3a1efe990da6225a3defa17934385c4be
madfury@ArchLinux <kelvinkarani11934@gmail.com>
2026-04-07 16:12:55 +0300

    Fix terminal toggle

--- nvim/.config/nvim/lua/plugins/fterm.lua
+++ nvim/.config/nvim/lua/plugins/fterm.lua
@@ -17,20 +17,33 @@ return {
 				fterm.open()
 			end, { desc = "Open FTerm" })
 
-			-- Close terminal
-			vim.keymap.set("n", "<leader>tc", function()
-				fterm.close()
-			end, { desc = "Close FTerm" })
-
-			-- Exit terminal (force close)
-			vim.keymap.set("n", "<leader>tx", function()
-				fterm.exit()
-			end, { desc = "Exit FTerm" })
-
-			-- Toggle terminal
+			-- Toggle terminal on
 			vim.keymap.set("n", "<leader>tt", function()
 				fterm.toggle()
 			end, { desc = "Toggle FTerm" })
+
+			-- Toggle terminal off
+			vim.keymap.set(
+				"t",
+				"<leader>tt",
+				[[<C-\><C-n><CMD>lua require("FTerm").toggle()<CR>]],
+				{ desc = "Toggle FTerm (terminal mode)" }
+			)
+
+			-- Close terminal 
+			vim.keymap.set(
+				"t",
+				"<leader>tc",
+				[[<C-\><C-n><CMD>lua require("FTerm").close()<CR>]],
+				{ desc = "Toggle FTerm (terminal mode)" }
+			)
+			-- Exit terminal 
+			vim.keymap.set(
+				"t",
+				"<leader>tx",
+				[[<C-\><C-n><CMD>lua require("FTerm").exit()<CR>]],
+				{ desc = "Toggle FTerm (terminal mode)" }
+			)
 		end,
 	},
 }
